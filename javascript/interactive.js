00e0469f3e0a0f9a4a538163cbe19c82155d9b33
Olane123 <olaneyt3@gmail.com>
2025-10-25 17:54:32 +0500

    javascript

---
+++ javascript/interactive.js
@@ -0,0 +1,24 @@
+﻿const nameTodoInput = document.getElementById("input__noteName")
+const addTodoButton = document.getElementById("input_addTodo")
+const todoContainer = document.getElementById("notesField")
+
+const todoList = []
+
+addTodoButton.addEventListener("click", () => {
+    if (nameTodoInput.value.trim()) {
+        todoList.push(nameTodoInput.value)
+        nameTodoInput.value = "";
+
+        todoContainer.innerHTML = "";
+        todoList.forEach((todo) =>
+        {
+            const todoElement = document.createElement("div");
+            todoElement.textContent = todo;
+            todoContainer.append(todoElement);
+        })
+    }
+})
+todoContainer.addEventListener('input', function() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+});
